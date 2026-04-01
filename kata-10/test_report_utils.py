7274a89de518af5bb334644eb805b17f4e97c5bb
kemolisticedu7-lang <kemolisticedu7@gmail.com>
2026-03-31 22:17:55 -0500

    Add failing test for medium risk classification

--- kata-10/test_report_utils.py
+++ kata-10/test_report_utils.py
@@ -3,3 +3,7 @@ from report_utils import classify_delay_risk
 
 def test_classify_low_risk():
     assert classify_delay_risk(0.2) == "low"
+
+
+def test_classify_medium_risk():
+    assert classify_delay_risk(0.5) == "medium"
